01d6112bd261b70438e77534e9acff4a16f0959d
taefreeze <potaekingdom@hotmail.com>
2021-02-15 16:41:50 +0700

    Change Background Option

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let changeColor = document.getElementById("changeColor");
+let textHL = document.getElementById("search");
 
 chrome.storage.sync.get("color", ({ color }) => {
     changeColor.style.backgroundColor = color;
@@ -17,4 +18,24 @@ function setPageBackgroundColor() {
     chrome.storage.sync.get("color", ({ color }) => {
         document.body.style.backgroundColor = color;
     });
+}
+
+function highlight(text) {
+    var inputText = document.getElementById("inputText");
+    var innerHTML = inputText.innerHTML;
+    var index = innerHTML.indexOf(text);
+    if (index >= 0) {
+        innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + text.length) + "</span>" + innerHTML.substring(index + text.length);
+        inputText.innerHTML = innerHTML;
+    }
+}
+
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
 }
