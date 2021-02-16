6bb44cc173b49e82657a8934d9a57ae67176b3cb
taefreeze <potaekingdom@hotmail.com>
2021-02-16 16:52:42 +0700

    copy ok

--- popup.js
+++ popup.js
@@ -15,3 +15,28 @@ copy.addEventListener('click', function(e) {
         answer.innerHTML = 'Unsupported Browser!';
     }
 });
+
+function copyIsSuccess() {
+    var copy
+    try {
+        copy = window.getSelection().toString();
+        copied = document.execCommand("copy")
+    } catch (e) {
+        copied = e;
+    }
+    return copy
+}
+
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text();
+    }
+    return text;
+}
+
+document.onmouseup = document.onkeyup = document.onselectionchange = function() {
+    document.getElementById("sel").value = copyIsSuccess();
+};
